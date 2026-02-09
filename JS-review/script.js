bc506b6ecba7ed6e358b440c757b88539d7ea9d4
Fett <gabfett@gmail.com>
2026-02-09 17:57:55 -0300

    Relembrando o uso do Fetch
    Temos duas formas aqui que mostram o uso do fetch, como a forma de leitura do js.
    
    a baixo podemos ver que a demonstração ficou mais aprofundada com o uso de async function

--- JS-review/script.js
+++ JS-review/script.js
@@ -363,8 +363,16 @@ const booksAfterUpdate = booksAfterDelete.map((book) => book.id === 1 ? { ...boo
 booksAfterUpdate;
 */
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("jonas");
+// console.log("jonas");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
