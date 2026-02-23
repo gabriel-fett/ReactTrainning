38c9c17e51cd8f5833cfa9cbd27bfe5c56e11955
Fett <gabfett@gmail.com>
2026-02-23 19:30:42 -0300

    Aula 39
    Inclusão de aperto ou fechado como alerta, e depois como status false/true no console

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -72,6 +72,16 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("Nós estamos abertos!");
+  // else alert("Estamos fechados!")
+
+
   return (
     <footer> {new Date().toLocaleTimeString()}. Nós estamos abertos!</footer>
   );
