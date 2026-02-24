0f8ee8e62405bfa6ca9256e59b49f940c40fd6e8
Fett <gabfett@gmail.com>
2026-02-24 14:08:37 -0300

    Aula 42 - props
    Incio do uso de props no projeto e estilizações dos componentes das pizzas

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -70,12 +70,36 @@ function Header() {
 function Menu() {
   return (
     <main className="menu">
-      <h2>Nosso Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -93,16 +117,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}. Nós estamos abertos!
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci"></img>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
