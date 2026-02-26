5fc1bede47f3cf22805c4121f0855242c2c7276e
Fett <gabfett@gmail.com>
2026-02-26 17:56:01 -0300

    Aula 47
    Configuração de fechado e aberto com com uma logica simples de aparecer ou não o menu quando estivermos fora do horario.

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -68,16 +68,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
-
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -122,7 +127,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. Nós estamos abertos!
+      {isOpen && (
+        <div className="order">
+          <p>We´re open util {closeHour}:00. Come visit us or order online!</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
