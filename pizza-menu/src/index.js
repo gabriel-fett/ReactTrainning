ce8f4ffb3fafcab7d2707ab9535849a7c9d7efa3
Fett <gabfett@gmail.com>
2026-03-01 13:07:12 -0300

    Aula 52
    Aprendendo a utilizar e quando utilizar o React Fragment
    
    <></>

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -77,14 +77,20 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p> Authentic italian cuisine. 6 creative dishes to choose from. All from
+            our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p> We're still working on our menu. Please come back later :) </p>
       )}
+
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
